4643cc03ccdba1bfabe20e4711dd258ea742b0c5
Katrine M S Thomsen <kath23ag@student.cbs.dk>
2024-11-28 10:44:11 +0100

    hej

--- public/js/login.js
+++ public/js/login.js
@@ -13,37 +13,65 @@ loginKnap.addEventListener("click", async () => {
     alert("Fyld email og password feltererne ud");
     return;
   }
-
-  //Laver et objekt med email og password
-  const loginData = {
-    email,
-    password,
-  };
-
-  fetch("/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(loginData),
-  })
-    //Hvis responsen ikke er ok, vises der en fejl.
-    .then((response) => {
-      if (!response.ok) {
-        return response.json().then((data) => {
-          throw new Error(data.error || "Fejl ved login");
-        });
-      }
-      return response.json();
-    })
-    .then((userData) => {
-      //Gemmer brugerens data i session storage
-      sessionStorage.setItem("user", JSON.stringify(userData));
-      alert("Du er nu logget ind");
-      window.location.href = "/stempel.html";
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      alert(error.message);
+  try {
+    //Step 1: her laver vi login anmodningen
+    const loginData = {
+      email,
+      password,
+    };
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),
     });
+
+    if (!response.ok) {
+      const data = await response.json();
+      throw new Error(data.error || "Fejl ved login");
+    }
+    //Gemmer brugerens data i en variabel, så vi kan gemme i session storage
+    const userData = await response.json();
+
+    //Step 2: Nu sendes bekræftelses koden til brugeren via SMS
+    const authResponse = await fetch("/authenticateUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    if (!authResponse.ok) {
+      const data = await authResponse.json();
+      throw new Error(
+        data.error || "Fejl ved autentificering, beskeden blev ikke sendt"
+      );
+    }
+    alert("Beskræftelseskode er sendt til din telefon");
+
+    //Step 3: Brugeren indtaster bekræftelseskoden, og vi tjekker om den er korrekt
+    const authCode = prompt("Indtast din bekræftelseskode");
+    const checkAuthCodeResponse = await fetch("/checkAuthCode", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, code: authCode }),
+    });
+
+    if (!checkAuthCodeResponse.ok) {
+      const data = await checkAuthCodeResponse.json();
+      throw new Error(data.error || "Fejl ved autentificering, forkert kode");
+    }
+    //Gemmer brugerens i session storage
+    sessionStorage.setItem("user", JSON.stringify(userData));
+
+    alert("Login succesfuld");
+    window.location.href = "/stempel.html";
+  } catch (error) {
+    console.error("Error:", error);
+    alert(error.message);
+  }
 });
